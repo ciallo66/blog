628f9b013ea34f3f79e00cc6165b4908fe13bdf8
abc114514abc <tianjiale5201314@gmail.com>
2026-05-05 20:49:26 +0800

    测试

--- lastnight.bat
+++ lastnight.bat
@@ -1,4 +1,4 @@
 @echo off
-for /f "tokens=1-3 delims=/- " %%a in ('powershell -Command "(Get-Date).AddHours(-12).ToString('yyyy-MM-ddTHH:mm:ss+0800')"') do set newdate=%%a-%%bT%%c
+for /f "tokens=1-3 delims=/- " %%a in ('powershell -Command "(Get-Date).AddHours(-4).ToString('yyyy-MM-ddTHH:mm:ss+0800')"') do set newdate=%%a-%%bT%%c
 git commit --amend --date="%newdate%" --no-edit
-echo 已改为当前时间减12小时
+echo 已改为当前时间减4小时
