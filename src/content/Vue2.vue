05389ae11bed2b139bb6eb39df886c4f9623dfd5
abc114514abc <tianjiale5201314@gmail.com>
2026-05-14 00:09:54 +0800

    。

---
+++ src/content/Vue2.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="aaa">
+        <br>
+        <router-link to='/blog/1'></router-link>
+        <h1>你好，你能看到我吗
+            这下好了可以随便写文章了
+        </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Vue2'
+    }
+</script>
+
+<style scoped>
+    .aaa{
+        width:50vw;
+        margin: auto;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import BlogList from '../views/BlogList.vue'
-import BlogPost from '../views/BlogPost.vue'
+
 import About from '../views/About.vue'
+
+import Vue2 from '../content/Vue2.vue'
+
 
 Vue.use(VueRouter)
 
@@ -14,8 +17,9 @@ const router = new VueRouter({
   routes:[
     { path: '/', name: 'Home', component: Home },
     { path: '/blog', name: 'BlogList', component: BlogList },
-    { path: '/blog/:id', name: 'BlogPost', component: BlogPost, props: true },
-    { path: '/about', name: 'About', component: About }
+
+    { path: '/about', name: 'About', component: About },
+    { path: '/blog/1',component:Vue2}
   ]
 })
 
