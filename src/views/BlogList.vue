9c469f1e73e87a0ae19f0cb7e985cf68497f1f13
abc114514abc <tianjiale5201314@gmail.com>
2026-05-18 21:27:15 +0800

    ？

--- src/views/BlogList.vue
+++ src/views/BlogList.vue
@@ -43,7 +43,8 @@ export default {
         { id: 2, title: 'md的学习', summary: 'md知识点总结', date: '2025-05-14' },
         { id: 3, title: 'Vue3的学习', summary: 'vue3的新内容', date: '2025-05-16' },
         { id: 4, title: '每天两个算法题', summary: '算法学习', date: '2025-05-16' },
-        { id: 5, title: 'promise异步', summary: '异步学习', date: '2025-05-17' },
+        { id: 5, title: '异步学习', summary: 'promise和async/await语法糖', date: '2025-05-17' },
+        { id: 6, title: 'ts学习', summary: '记录ts的部分语法', date: '2025-05-18' },
       ]
     }
   },
