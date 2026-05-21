c1f2e062b457a9615840d91c257ffdc63764f847
abc114514abc <tianjiale5201314@gmail.com>
2026-05-21 19:21:39 +0800

    跳转回到最上面

--- src/router/index.js
+++ src/router/index.js
@@ -7,11 +7,7 @@ import BlogList from '../views/BlogList.vue'
 import About from '../views/About.vue'
 import BlogPost from '../views/BlogPost.vue'
 
-
-
 Vue.use(VueRouter)
-
-
 
 const router = new VueRouter({
   mode: 'hash',
@@ -34,8 +30,13 @@ const router = new VueRouter({
       name: 'BlogPost',
       component: BlogPost
     }
-    
-  ]
+  ],
+
+  scrollBehavior(to, from, savedPosition) {
+      //  只要是正常点击链接跳转，通通无脑强制滚回最顶部！
+      // 注意：Vue2 必须写成 { x: 0, y: 0 }，写 top 和 left 浏览器不认哦！
+      return { x: 0, y: 0 }
+  }
 })
 
 export default router
