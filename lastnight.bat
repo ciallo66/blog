f52407f20c3e8e2e06dcda0492f9fcc521cea9b8
abc114514abc <tianjiale5201314@gmail.com>
2026-07-02 22:39:00 +0800

    总结

--- lastnight.bat
+++ lastnight.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell -Command "$d = (Get-Date).AddHours(-4).ToString('yyyy-MM-ddTHH:mm:ss+0800'); & git commit --amend --date=\"$d\" --no-edit"
+powershell -Command "$d = (Get-Date).AddHours(-12).ToString('yyyy-MM-ddTHH:mm:ss+0800'); & git commit --amend --date=\"$d\" --no-edit"
 echo 已改为当前时间减4小时
